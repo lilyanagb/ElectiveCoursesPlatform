36d9d8eb7c4f6d9b9ff3addbfed7428c0e4ab6bc
Lilyana <lilyanagb@abv.bg>
2024-01-13 20:06:31 +0200

    Remove unused libraries

--- src/main/java/com/example/demo/repository/EnrollmentRepository.java
+++ src/main/java/com/example/demo/repository/EnrollmentRepository.java
@@ -1,13 +1,9 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.Course;
 import com.example.demo.model.Enrollment;
-import com.example.demo.model.User;
 
 @Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
--- src/main/java/com/example/demo/repository/TeacherRepository.java
+++ src/main/java/com/example/demo/repository/TeacherRepository.java
@@ -1,11 +1,8 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.Course;
 import com.example.demo.model.Teacher;
 
 @Repository
